4e349600475dbee16bf8ce3729b9781475515723
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-10 16:49:04 +0300

    added logic for 2 digits and equalityButton

--- script.js
+++ script.js
@@ -13,19 +13,100 @@ function divide(a, b) {
 }
 
 function operate(operator, num1, num2) {
-   switch (operator.toString()) {
-      case '+': console.log(add(num1, num2))
+   switch (operator) {
+      case '+':
+         firstValue = add(+num1, +num2)
+         display.textContent = firstValue
          break;
-      case '-': console.log(subtract(num1, num2))
+      case '-':
+         firstValue = subtract(+num1, +num2)
+         display.textContent = firstValue
          break;
-      case '*': console.log(multiply(num1, num2))
+      case 'x':
+         firstValue = multiply(+num1, +num2)
+         display.textContent = firstValue
          break;
-      case '/': console.log(divide(num1, num2))
+      case '/':
+         firstValue = divide(+num1, +num2)
+         display.textContent = firstValue
          break;
    }
 }
+/* ------------------------------ */
+let firstValue = ''
+let secondValue = ''
+let mathSign = ''
+/* ------------------------------ */
 
-operate('+', 10, 20)
-operate('-', 10, 20)
-operate('*', 10, 20)
-operate('/', 10, 20)
+let display = document.querySelector('.display')
+let digits = document.querySelector('.digits-btns')
+/* ------------------------------ */
+
+digits.addEventListener('click', (e) => {
+   if (e.target.tagName != 'BUTTON') {
+      return
+   }
+   if (display.textContent == 0) {
+      display.textContent = ''
+   }
+
+   if (secondValue == '' && mathSign == '') {
+      firstValue += e.target.value
+      display.textContent = firstValue
+   } else if (firstValue !== '' && secondValue !== '') {
+
+   }
+   else {
+      secondValue += e.target.value
+      display.textContent = secondValue
+   }
+
+   console.log(firstValue, mathSign, secondValue)
+   return
+
+})
+/*-----------------------------*/
+
+let math = document.querySelector('.math-btns')
+/* ------------------------------ */
+
+math.addEventListener('click', (e) => {
+   // console.log(e.target.id)
+   switch (e.target.id) {
+      case 'reset':
+         firstValue = '';
+         secondValue = '';
+         mathSign = '';
+         display.innerHTML = 0;
+         break
+      case 'add':
+         mathSign = e.target.value
+         display.textContent = mathSign
+         console.log(firstValue, mathSign, secondValue)
+         break
+      case 'sub':
+         mathSign = e.target.value
+         display.textContent = mathSign
+         console.log(firstValue, mathSign, secondValue)
+         break
+      case 'mult':
+         mathSign = e.target.value
+         display.textContent = mathSign
+         console.log(firstValue, mathSign, secondValue)
+         break
+      case 'divide':
+         mathSign = e.target.value
+         display.textContent = mathSign
+         console.log(firstValue, mathSign, secondValue)
+         break
+      case 'equal':
+         operate(mathSign, firstValue, secondValue)
+
+         console.log(firstValue, mathSign, secondValue)
+
+         break
+
+   }
+
+}
+)
